3ff5bd3762593cabb4604f354d8ba9ae2b86cd17
Holben888 <holben888@gmail.com>
2018-11-20 13:24:28 -0500

    add splash screen, rules screen

---
+++ images/rulesScreen.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.7
+ * Invocation command was nin10kit --mode=3 rulesScreen ./images/rules_background.png 
+ * Time-stamp: Tuesday 11/20/2018, 13:13:37
+ * 
+ * Image Information
+ * -----------------
+ * ./images/rules_background.png 240@160
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef RULESSCREEN_H
+#define RULESSCREEN_H
+
+extern const unsigned short rules_background[38400];
+#define RULES_BACKGROUND_SIZE 76800
+#define RULES_BACKGROUND_LENGTH 38400
+#define RULES_BACKGROUND_WIDTH 240
+#define RULES_BACKGROUND_HEIGHT 160
+
+#endif
+
---
+++ images/splashScreen.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.7
+ * Invocation command was nin10kit --mode=3 splashScreen ./images/splash_screen.png 
+ * Time-stamp: Tuesday 11/20/2018, 12:23:12
+ * 
+ * Image Information
+ * -----------------
+ * ./images/splash_screen.png 240@160
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef SPLASHSCREEN_H
+#define SPLASHSCREEN_H
+
+extern const unsigned short splash_screen[38400];
+#define SPLASH_SCREEN_SIZE 76800
+#define SPLASH_SCREEN_LENGTH 38400
+#define SPLASH_SCREEN_WIDTH 240
+#define SPLASH_SCREEN_HEIGHT 160
+
+#endif
+
